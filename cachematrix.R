b814dc48133b061c7ad0277bad527f7e4c1be755
doubima <mingjunma81@gmail.com>
2016-10-08 21:26:09 -0400

    Test change

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        #cached inverse of matrix
+        inv <- NULL
+        
+        # getter and setter for matrix
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv<<- inverse 
+        getInverse <- function() inv
+        
+        #return list of functions for matrix
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
@@ -12,4 +27,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        #return cached matrix inverse if it's been already computed
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        
+        #cache inverse
+        x$setInverse(inv)
+        
+        #return inverse of matrix
+        return(inv)
 }
